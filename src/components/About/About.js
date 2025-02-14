bf2f6f56675e2afc525e557e7d02a389e0e6c4e9
damedotblog <hi@dame.art>
2025-02-14 01:46:31 -0500

    search bar styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <>
       <main className="about-page">
-      <div className="about-content">
+      <div className="alt-card">
         <h1>About cred.blue</h1>
         <p>
           Unlike social media scores that have predated it, cred.blue is <strong>not</strong> a measurement of social influence or popularity. Instead, it aims to measure an account’s general reputation or footprint within the Atmosphere community, regardless of how many followers it has.
