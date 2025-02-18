13ccbd4ccebac51c355ce38c4efc74980ef8d470
damedotblog <hi@dame.art>
2025-02-18 16:34:17 -0500

    remove analytics for now

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Analytics } from "@vercel/analytics/react"
 import { AuthProvider } from './AuthContext'; // Ensure correct path
 import { ThemeProvider } from './contexts/ThemeContext'; // Adjust the path as necessary
 import "./index.css";
@@ -13,12 +12,10 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Analytics>
     <AuthProvider>
     <ThemeProvider>
       <App />
     </ThemeProvider>
     </AuthProvider>
-    </Analytics>
   </React.StrictMode>
 );
