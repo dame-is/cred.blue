98efb20ee6d000c9a4cbb6dffe410b89b49fb323
damedotblog <hi@dame.art>
2025-02-20 05:38:48 -0500

    add newsletter

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,20 +1,50 @@
 // src/components/Newsletter/Newsletter.jsx
-
 import React from 'react';
 import './Newsletter.css';
 
 const Newsletter = () => {
   return (
-    <>
-      <main className="newsletter-page">
+    <main className="newsletter-page">
       <div className="alt-card">
         <h1>Subscribe to the Newsletter</h1>
-        <p>
-          Lorem ipsum...
-        </p>
-      </div> 
-      </main>
-    </>
+        <p>Lorem ipsum...</p>
+        
+        <form 
+          className="newsletter-form embeddable-buttondown-form"
+          action="https://buttondown.com/api/emails/embed-subscribe/dame"
+          method="post"
+          target="popupwindow"
+          onSubmit={() => window.open('https://buttondown.com/dame', 'popupwindow')}
+        >
+          <label htmlFor="bd-email">Email Address</label>
+          <input 
+            type="email" 
+            name="email" 
+            id="bd-email" 
+            placeholder="username@gmail.com" 
+          />
+
+          <label htmlFor="bsky-handle">Bluesky Handle (Optional)</label>
+          <input 
+            type="text" 
+            name="metadata__bsky-handle" 
+            id="bsky-handle" 
+            placeholder="username.bsky.social" 
+          />
+
+          <label htmlFor="name">Name or Pseudonym (Optional)</label>
+          <input 
+            type="text" 
+            name="metadata__name" 
+            id="name" 
+            placeholder="name" 
+          />
+
+          <input type="submit" value="Subscribe" />
+          <input type="hidden" name="tag" value="cred.blue subscribers" />
+        </form>
+      </div>
+    </main>
   );
 };
 
