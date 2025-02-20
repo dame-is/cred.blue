41190accd9d1d007410145fdc68382053cb04c87
damedotblog <hi@dame.art>
2025-02-20 00:12:24 -0500

    add chart to method page

--- src/components/ScoringMethodology/ScoringMethodology.js
+++ src/components/ScoringMethodology/ScoringMethodology.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './ScoringMethodology.css';
+import ScoreGauge from './ScoreGauge';
 
 const ScoringMethodology = () => {
   return (
     <main className="methodology-page">
       <div className="alt-card">
         <h1>The Scoring Methodology</h1>
+
+        <div className="methodology-page-chart">
+              <ScoreGauge score={634} />
+            </div>
+
         <p>Your cred.blue score is generated based on two major categories...</p>
         
         <h3>1. Bluesky Data</h3>
@@ -40,8 +46,8 @@ const ScoringMethodology = () => {
         </p>
 
         <p>
-          A score between 0 - 300 likely indicates that an account is either very new 
-          to the network or isn't very active. A score of 300 - 700 is within a "healthy" range. Scores that are 700+ typically indicate accounts that have been around awhile and are very active. The different score ranges are still in early development along with the algorithm, so these details are likely to change.
+          A score between 0-300 likely indicates that an account is either very new 
+          to the network or isn't very active. A score of 300-700 is within a "healthy" range. Scores that are 700+ typically indicate accounts that have been around awhile and are very active. The different score ranges are still in early development along with the algorithm, so these details are likely to change.
         </p>
 
         <h2>What are the different social statuses?</h2>
