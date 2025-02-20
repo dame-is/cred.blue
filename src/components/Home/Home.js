1582f02ca632c776e26134819d93446414b6d5cf
damedotblog <hi@dame.art>
2025-02-20 07:39:47 -0500

    fix atproto spelling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,11 +13,11 @@ const Home = () => {
     <Helmet>
     {/* Default meta tags */}
     <title>cred.blue</title>
-    <meta name="description" content="Generate a Bluesky credibility score. Understand your Atproto data footprint. Vibe check strangers and new accounts." />
+    <meta name="description" content="Generate a Bluesky credibility score. Understand your AT Proto data footprint. Vibe check strangers and new accounts." />
     
     {/* OpenGraph Meta Tags */}
     <meta property="og:title" content="cred.blue" />
-    <meta property="og:description" content="Generate a Bluesky credibility score. Understand your Atproto data footprint. Vibe check strangers and new accounts." />
+    <meta property="og:description" content="Generate a Bluesky credibility score. Understand your AT Proto data footprint. Vibe check strangers and new accounts." />
     <meta property="og:image" content="https://cred.blue/cred-blue-banner.jpg" />
     <meta property="og:url" content="https://cred.blue" />
     <meta property="og:type" content="website" />
@@ -25,7 +25,7 @@ const Home = () => {
     {/* Twitter Card Meta Tags */}
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="cred.blue" />
-    <meta name="twitter:description" content="Generate a Bluesky credibility score. Understand your Atproto data footprint. Vibe check strangers and new accounts." />
+    <meta name="twitter:description" content="Generate a Bluesky credibility score. Understand your AT Proto footprint. Vibe check strangers and new accounts." />
     <meta name="twitter:image" content="https://cred.blue/cred-blue-banner.jpg" />
   </Helmet>
 
