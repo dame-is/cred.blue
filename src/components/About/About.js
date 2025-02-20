7494fe7c8dda0cee7df08665cd8e8ae3c21da0bf
damedotblog <hi@dame.art>
2025-02-19 20:16:37 -0500

    update about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,13 +14,14 @@ const About = () => {
             <li>Understand your AT Proto data footprint.</li>
             <li>Vibe check strangers and new accounts.</li>
           </ul>
-
-          <h2>About</h2>
+          <h2>Intro</h2>
           <p>The <strong>cred.blue</strong> platform allows anyone to better understand the data footprint that an <strong>AT Protocol</strong> or <strong>Bluesky</strong> identity is creating. During its beta launch, <strong>cred.blue</strong> provides an early version of its custom scoring algorithm that generates a "credibility" score based on an identity's activity in the ecosystem. Additionally, there is an <strong>Alt Text Rating Tool</strong> that allows you to see how consistently an account includes accessibility alt-text when posting images on Bluesky.</p>
           
           <p>The <strong>cred.blue</strong> scoring methodology is intended to provide a helpful contextual analysis of the public data associated with an AT Protocol or Bluesky identity. The hope is that the scoring system might help incentivize healthier behaviors and activity patterns on the network.</p>
           
           <p>Future versions of cred.blue may include authenticated data analysis (for your personal AT Proto data that isn't public), a custom score/credibility tracking lexicon, and an in-app labeler for the Bluesky platform.</p>
+
+          <a href="https://bsky.app/profile/did:plc:7lazllqgiktcts3gs4i6xtv6" target="_blank" rel="noreferrer">Follow cred.blue on Bluesky</a>
 
           <h2>FAQs</h2>
           
