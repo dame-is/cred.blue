6000a762068e74d5b2d9e734752f74ba54b2f58f
damedotblog <hi@dame.art>
2025-02-24 13:19:29 -0500

    add shortcut stuff

--- src/components/Shortcut/Shortcut.js
+++ src/components/Shortcut/Shortcut.js
@@ -1,5 +1,4 @@
 // src/components/Shortcut/Shortcut.jsx
-
 import React from 'react';
 import './Shortcut.css';
 
@@ -7,29 +6,42 @@ const Shortcut = () => {
   return (
     <>
       <main className="shortcut-page">
-       <div className="alt-card">
-        <h1>Download Apple Shortcut</h1>
-
-          <p>If you have an iPhone, iPad, or Macbook, you can download a special Apple Shortcut to your device that will allow you to quickly check a Bluesky account's cred.blue score while you're scrolling inside of the Bluesky app.
+        <div className="alt-card">
+          <h1>Download Apple Shortcut</h1>
+          <p>
+            If you have an iPhone, iPad, or Macbook, you can download a special Apple Shortcut to your device that will allow you to quickly check a Bluesky account's cred.blue score while you're scrolling inside of the Bluesky app.
           </p>
-          
-          <p>Thank you for considering supporting me,</p>
 
-          <p>Dame</p>
-          <p><a href="https://bsky.app/profile/dame.is" target="_blank" rel="noreferrer">@dame.is</a></p>
+          <div className="image-container">
+            <img
+              src="/how-to-use-cred-blue-shortcut.png"
+              alt="How to use Cred.blue Shortcut"
+              className="shortcut-image"
+            />
+          </div>
 
           <div className="shortcut-buttons">
-          <button
+            <button
               className="patreon-button"
               type="button"
-              onClick={() => window.open(
-                `https://cred.blue/shortcut`, '_blank'
-              )}
-              >
-                Download Shortcut 
-              </button>
+              onClick={() => window.open('https://cred.blue/shortcut', '_blank')}
+            >
+              Download Shortcut
+            </button>
           </div>
-       </div> 
+
+          <p>
+            Once you have the Apple Shortcut installed, go through the setup process to enter your username and then check to make sure that the Share Sheet feature has been enabled.
+          </p>
+
+          <div className="image-container">
+            <img
+              src="/enable-share-sheet.png"
+              alt="Enable Share Sheet"
+              className="shortcut-image"
+            />
+          </div>
+        </div>
       </main>
     </>
   );
