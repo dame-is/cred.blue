0d2283e4d72e5b1909c60d4efcf6b4512b24c5cf
damedotblog <hi@dame.art>
2025-02-27 14:16:01 -0500

    add resource loader

---
+++ src/components/Resources/ResourceLoader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResourceLoader = () => {
+  return (
+    <div className="resource-loader">
+      <div className="loader-spinner"></div>
+      <p>Loading resources...</p>
+    </div>
+  );
+};
+
+export default ResourceLoader;
