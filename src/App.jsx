3c6b6f6b4e7557850d8d9ba68bc6a2320c16b6f6
damedotblog <hi@dame.art>
2025-03-03 14:45:10 -0500

    add admin to testing

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,9 @@ const App = () => {
               <Route path="/zen" element={<ZenPage />} />
               <Route path="/methodology" element={<ScoringMethodology />} />
               
+              {/* Admin Route */}
+              <Route path="/admin" element={<AdminRoute />} />
+              
               {/* Handle both DIDs and regular usernames */}
               <Route path="/:username" element={<UserProfile />} />
               
